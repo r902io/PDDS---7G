19c09b1a57427667a76ec3c3bdb83440f81c5cc6
r902io <a20210905@pucp.edu.pe>
2026-09-06 20:15:11 -0500

    FEATURE: nuevecito de paquete

---
+++ Backend-Planificador-SISRAP/sisrap-backend/src/main/java/pe/edu/pucp/sisrap/sisrap_backend/SisrapBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.sisrap.sisrap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisrapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisrapBackendApplication.class, args);
+	}
+
+}
---
+++ Backend-Planificador-SISRAP/sisrap-backend/src/test/java/pe/edu/pucp/sisrap/sisrap_backend/SisrapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.sisrap.sisrap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisrapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
